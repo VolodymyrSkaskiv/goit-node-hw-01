e5355eb7991cdce399fab0eb0f5e818e9db826c8
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-06-25 23:16:22 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,6 +1,8 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
+const nanoid = require("nanoid");
+
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
